9b9583a1ed4697cfc0553d2b5ee942eae06c817d
sadique-cws <sadique.hussain96@gmail.com>
2020-10-07 09:07:05 +0530

    design change

--- application/views/public/header.php
+++ application/views/public/header.php
@@ -5,28 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OldSale</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="<?= base_url('upload/css/style.css');?>"/>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-theme fixed-top">
     <a href="<?= base_url('home/index');?>" class="navbar-brand">OldSale</a>
 
     <form action="<?= base_url('home/search');?>" class="form-inline mx-auto" method="get">
-        <input type="search" name="search" class="form-control">
-        <input type="submit" name="find" class="btn btn-danger">
+        <input type="search" name="search" class="form-control border-0 rounded-0" size="80" placeholder="Search Product">
+        <button type="submit" name="find" class="btn bg-white rounded-0"><i class="fas fa-search"></i></button>
     </form>
 
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item"><a href="<?= base_url('home/index');?>" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="<?= base_url('home/addPost');?>" class="btn btn-dark">Sell</a></li>
+        <li class="nav-item"><a href="<?= base_url('home/index');?>" class="nav-link active">Home</a></li>
+        <li class="nav-item"><a href="<?= base_url('home/index');?>" class="nav-link active"><i class="fas fa-user"></i> Login</a></li>
+        <li class="nav-item"><a href="<?= base_url('home/index');?>" class="nav-link active">Signup</a></li>
+        <li class="nav-item"><a href="<?= base_url('home/addPost');?>" class="btn btn-light">Sell <i class="fas fa-mouse-pointer"></i></a></li>
     </ul>
 </nav>
 
+<br>
+<br>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm mt-2">
     <ul class="navbar-nav">
     <?php 
     foreach($category as $cat):?>
-        <li class="nav-item  mr-2"><a href="<?= base_url('home/category/'.$cat->id);?>" class="badge badge-primary badge-pill p-2"><?= $cat->title;?></a></li>
+        <li class="nav-item  mr-2"><a href="<?= base_url('home/category/'.$cat->id);?>" class="badge bg-purple text-white badge-pill p-2"><?= $cat->title;?></a></li>
     <?php endforeach;?>
     </ul>
 </nav>
--- application/views/public/index.php
+++ application/views/public/index.php
@@ -3,13 +3,16 @@
     <div class="row">
     <?php 
     foreach($products as $pro): ?>
-        <div class="col-lg-3">
-            <div class="card">
+        <div class="col-lg-3 mb-3">
+            <div class="card border-0 shadow-sm">
             <a href="<?= base_url('home/product/'.$pro->id);?>">
-                <img src="<?= base_url("upload/".$pro->image);?>" alt="" class="w-100" style="object-fit:cover;height:220px">
+                <img src="<?= base_url("upload/".$pro->image);?>" alt="" class="w-100 border border-light" style="object-fit:cover;height:220px">
             </a>
-                <div class="card-body">
-                    <h2 class="lead text-truncate"><?= $pro->title;?></h2>
+                <div class="card-body p-2">
+                    <h2 class="h6 pt-0 text-capitalize font-theme text-truncate"><?= $pro->title;?></h2>
+                    <p class="small text-muted p-0 m-0 mt-n2"><?= $pro->brand;?></p>
+                    <p class=" p-0 m-0 mt-1 font-weight-bold text-theme d-inline-block">₹<?= $pro->price;?>/-</p>
+                    <p class=" p-0 m-0 mt-1 float-right small text-muted d-inline-block"><span><i class="fas fa-map-marker-alt"></i> </span><?= $pro->city;?></p>
                 </div>
             </div>
         </div>
