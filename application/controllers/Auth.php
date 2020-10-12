48933afe7b435b745590d3cb45e3a394c6646d88
sadique-cws <sadique.hussain96@gmail.com>
2020-10-12 09:16:02 +0530

    login and signup

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,13 +4,59 @@
 class Auth extends CI_Controller{
     public function login(){
         $data['category'] = $this->db->get('category')->result();
+        
+        $this->form_validation->set_rules('contact','contact','required');
+        $this->form_validation->set_rules('password','password','required');
 
-        $this->load->view("public/header",$data);
-        $this->load->view("auth/login");
-        $this->load->view("public/footer");
+        if($this->form_validation->run()){
+            $data = [
+                'contact' => $_POST['contact'],
+                'password' => $_POST['password']
+            ];
+
+            $count = $this->db->where($data)->get('accounts')->num_rows();
+
+            if($count > 0){
+                $this->session->set_userdata("user",$_POST['contact']);
+                redirect("home/index");
+            }
+            else{
+                $this->session->set_flashdata("error","incorrect password and username");
+                redirect("auth/login");
+            }
+        }
+        else{
+            $this->session->set_flashdata("error","login invalid");
+            $this->load->view("public/header",$data);
+            $this->load->view("auth/login");
+            $this->load->view("public/footer");
+        }
     }
     public function signup(){
         $data['category'] = $this->db->get('category')->result();
+
+        $this->form_validation->set_rules('name','name','required');
+        $this->form_validation->set_rules('contact','contact','required');
+        $this->form_validation->set_rules('email','email','required');
+        $this->form_validation->set_rules('city','city','required');
+        $this->form_validation->set_rules('password','password','required');
+        $this->form_validation->set_rules('confirm_password','re-password','required|matches[password]');
+
+
+        if($this->form_validation->run()){
+            $data = [
+                'name' => $_POST['name'],
+                'contact' => $_POST['contact'],
+                'email' => $_POST['email'],
+                'city' => $_POST['city'],
+                'password' => md5($_POST['password']),
+            ];
+
+            $this->db->insert('accounts',$data);
+            //login after signup
+            $this->session->set_userdata("user",$_POST['contact']);
+            redirect("auth/login");
+        }
 
         $this->load->view("public/header",$data);
         $this->load->view("auth/signup");
--- application/views/auth/signup.php
+++ application/views/auth/signup.php
@@ -4,30 +4,36 @@
             <div class="card">
                 <div class="card-body">
                 <h4 class="h5">Create an account 100% free</h4>
-                    <form action="" method="post">
+                    <form action="<?= base_url('auth/signup');?>" method="post">
                         <div class="form-group">
                             <label for="">Name</label>
                             <input type="text" name="name" class="form-control">
+                            <?= form_error('name');?>
                         </div>
                         <div class="form-group">
                             <label for="">contact</label>
                             <input type="text" name="contact" class="form-control">
+                            <?= form_error('contact');?>
                         </div>
                         <div class="form-group">
                             <label for="">email</label>
                             <input type="text" name="email" class="form-control">
+                            <?= form_error('email');?>
                         </div>
                         <div class="form-group">
                             <label for="">city</label>
                             <input type="text" name="city" class="form-control">
+                            <?= form_error('city');?>
                         </div>
                         <div class="form-group">
                             <label for="">password</label>
                             <input type="text" name="password" class="form-control">
+                            <?= form_error('password');?>
                         </div>
                         <div class="form-group">
                             <label for="">confirm_password</label>
                             <input type="text" name="confirm_password" class="form-control">
+                            <?= form_error('confirm_password');?>
                         </div>
                         <div class="form-group">
                             <input type="submit" class="btn  bg-theme text-white btn-block">
